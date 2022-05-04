a7006e6a77925df31e62fee41d834697c457d6e0
MikeMNelhams <oa18502@bristol.ac.uk>
2022-05-04 20:36:52 +0100

    Changelist: - Broke Philippe's code ;D

--- cGAN.py
+++ cGAN.py
@@ -1,15 +1,27 @@
-from gans import combine_gan, generate_real_samples, sample
-from numpy.random import rand, randn, random, randint
-import tensorflow as tf
+import keras.losses
+
+from gans import combine_gan, generate_real_samples, generate_fake_samples, generate_latent_points, keras, generator, discriminator
+from keras import backend, Model
+import pandas as pd
 import numpy as np
 
 
+class HyperParameters:
+    def __init__(self, latent_dim: int=50, learning_rate=1e-5):
+        self.latent_dim = latent_dim
+        self.learning_rate = learning_rate
+
+
 class ConditionalGAN:
-    def __init__(self, g_model, d_model, labels, hyper_parameters):
+    def __init__(self, g_model, d_model, labels, hyper_parameters: HyperParameters()):
         self.labels = labels
         self.generator = g_model
         self.discriminator = d_model
         self.hyper_parameters = hyper_parameters
+
+        opt = keras.optimizers.Adam(lr=hyper_parameters.learning_rate)
+        self.generator.compile(loss="binary_crossentropy", optimizer=opt, metrics='accuracy')
+
         self.model = combine_gan(g_model, d_model)
 
     def train(self, data, epochs: int=100, batch_size: int=64):
@@ -17,32 +29,77 @@ class ConditionalGAN:
         half_batch = int(batch_size / 2)
 
         for i in range(epochs):
-            # enumerate batches over the training set
             for j in range(bat_per_epo):
-                # get randomly selected 'real' samples
-                [X_real, labels_real], y_real = generate_real_samples(
-                    data, self.labels, half_batch)
-                # update discriminator model weights
-                [X_fake, labels], y_fake = generate_fake_samples2(
-                    g_model, laten_dim, half_batch)
-                d_loss1, _ = d_model.train_obatch_size([X_real, labels_real], y_real)
-                d_loss2, _ = d_model.train_obatch_size([X_fake, labels], y_fake)
-                # prepare points in latent space as input for the generator ##### z_input
-                [z_input, labels_input] = generate_latent_points2(
-                    laten_dim, batch_size)
-                y_gan = np.ones((batch_size, 1))
-                _, g_loss = gan_model.train_obatch_size(
-                    [z_input, labels_input], y_gan)
+                d_loss1, d_loss2, g_loss = self.train_mini_batch(data, batch_size, half_batch)
+                self.print_loss(i, j, bat_per_epo, d_loss1, d_loss2, g_loss)
+        self.save()
 
-                # summarize loss on this batch
-                print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
-                      (i + 1, j + 1, bat_per_epo, d_loss1, d_loss2, g_loss))
-        # save the generator model
-        g_model.save('cgan_generator2.h5')
-        
+    def train_mini_batch(self, data, batch_size: int, half_batch_size: int) -> (float, float, float):
+        [X_real, labels_real], y_real = generate_real_samples(data, self.labels, half_batch_size)
+        [X_fake, labels], y_fake = generate_fake_samples(self.generator, self.hyper_parameters.latent_dim, half_batch_size)
+        d_loss1, _ = self.discriminator.train_obatch_size([X_real, labels_real], y_real)
+        d_loss2, _ = self.discriminator.train_obatch_size([X_fake, labels], y_fake)
+        # prepare points in latent space as input for the generator
+        [z_input, labels_input] = generate_latent_points(self.hyper_parameters.latent_dim, batch_size)
+        y_gan = np.ones((batch_size, 1))
+        _, g_loss = self.generator.train_obatch_size([z_input, labels_input], y_gan)
+
+        return d_loss1, d_loss2, g_loss
+
+    def save(self) -> None:
+        self.generator.save('cgan_generator2.h5')
+        self.discriminator.save('cgan_discriminator2.h5')
+
     def feature_matching_loss(self, y_true, y_pred):
-        pass
+        """ Binary Cross Entropy Feature Matching Loss """
+        intermediate_layer_model = self.intermediate_layer()
+        f_x_given_y = intermediate_layer_model.predict(y_true)
 
-    def custom_loss(self, y_true, y_pred):#
-        pass
-        
+        f_g_of_z = intermediate_layer_model.predict(y_pred)
+
+        f_x_given_y = backend.cast_to_floatx(f_x_given_y)
+        f_g_of_z = backend.cast_to_floatx(f_g_of_z)
+
+        loss = backend.sqrt(backend.square(f_x_given_y - f_g_of_z))
+        loss = backend.sum(loss)
+        print(f"FM Loss: {loss}")
+        return loss
+
+    def custom_loss(self, y_true, y_pred):
+        fm_loss = self.feature_matching_loss(y_true, y_pred)
+        bce_d_loss = keras.losses.bce(y_true, y_pred)
+
+        final_loss = fm_loss + bce_d_loss
+        return final_loss
+
+    def intermediate_layer(self):
+        layer_name = 'intermediate_layer'
+        intermediate_layer_model = Model(inputs=self.discriminator.input,
+                                         outputs=self.discriminator.get_layer(layer_name).output)
+        return intermediate_layer_model
+
+    @staticmethod
+    def print_loss(i, j, bat_per_epo, d_loss1, d_loss2, g_loss):
+        print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
+              (i + 1, j + 1, bat_per_epo, d_loss1, d_loss2, g_loss))
+
+
+def main():
+    hyper_params = HyperParameters(latent_dim=50)
+
+    df = pd.read_csv(r"Q:\MichaelsStuff\EngMaths\Year4\AppliedDataScience\creditcard.csv")
+
+    columns_ = df.columns[2:30]
+    dataset = df[columns_]  # X
+    labels = df.Class  # y.Class
+
+    d_model = discriminator()
+    # create the generator
+    g_model = generator(hyper_params.latent_dim)
+
+    philippe = ConditionalGAN(d_model, g_model, labels=labels, hyper_parameters=hyper_params)
+    philippe.train(dataset)
+
+
+if __name__ == "__main__":
+    main()
--- gans.py
+++ gans.py
@@ -1,43 +1,19 @@
-import math
-from turtle import shape
-from sklearn.linear_model import LinearRegression
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from numpy.random import normal, randint
-from tensorflow.keras import layers
-from keras.models import load_model, Model
+from numpy.random import normal
+from keras import layers
+from keras.models import Model
 import numpy as np
-from numpy.random import rand, randn, random
 from numpy.random import randint
-import matplotlib.pyplot as plt
-import keras.backend as K
-from keras.utils.vis_utils import plot_model
 
 sample = ((tf.Tensor, np.array), np.array)
 
-# Whole dataFrame
-df = pd.read_csv('creditcard.csv')
 
-# # Import whole dataset
-# df_balanced = pd.read_csv('balanced_data.csv')
-
-# # Import balanced dataset
-# df_balanced = pd.read_csv('balanced_data.csv')
-# # Pick only the froud data
-# froud = df_balanced[df_balanced["Class"] == 1]
-# # pd datatrame of only the Vs
-# froud_V_colums = froud.columns[2:30]
-# print(froud_V_colums)
-# df_froud_Vs = froud[froud_V_colums]
-#####################
-df_10_per = df.sample(n=math.floor(len(df)/10))
-X, y = df_10_per.iloc[:, 1:-2], df_10_per.iloc[:, -1:]
-
-# Select real samples
+# df_10_per = df.sample(n=math.floor(len(df)/10))
+# X, y = df_10_per.iloc[:, 1:-2], df_10_per.iloc[:, -1:]
 
 
-def generate_real_samples2(dataset, labels, n_samples):
+def generate_real_samples(dataset, labels, n_samples):
     # split into images and labels
     # choose random instances
     idx = randint(0, dataset.shape[0], n_samples)
@@ -58,13 +34,14 @@ def generate_real_samples2(dataset, labels, n_samples):
     return [X, labels], y
 
 
-def generate_fake_samples2(generator, latent_dim, n_samples):
-    z_input, labels_input = generate_latent_points2(latent_dim, n_samples)
+def generate_fake_samples(g_model, latent_dim, n_samples):
+    z_input, labels_input = generate_latent_points(latent_dim, n_samples)
     # z_input = np.reshape(z_input, (32, -1))
     # print('z_input-------------')
     # print(z_input)
-
-    datapoints = generator.predict([z_input, labels_input])
+    print("Shapes:")
+    print(z_input.shape, labels_input.shape)
+    datapoints = g_model.predict([z_input, labels_input])
     # create class labels
     datapoints = tf.reshape(datapoints, [n_samples, 28])
     y = np.zeros((n_samples, 1))
@@ -105,7 +82,7 @@ def discriminator_model():
 # define the standalone discriminator model
 
 
-def define_discriminator(n_classes=2):  # in_shape=(1,28)
+def discriminator(n_classes: int=2):  # in_shape=(1,28)
 
     model = keras.Sequential(
         [
@@ -135,7 +112,7 @@ def define_discriminator(n_classes=2):  # in_shape=(1,28)
     return model_dis
 
 
-def define_generator(laten_dim, n_classes=2):
+def generator(laten_dim, n_classes=2):
 
     model = keras.Sequential(
         [
@@ -153,7 +130,6 @@ def define_generator(laten_dim, n_classes=2):
     model_input = layers.multiply([noise, label_embedding])
     gen_sample = model(model_input)
     model_f = Model([noise, label], gen_sample, name="Generator")
-    # define model
     return model_f
 
 
@@ -169,14 +145,10 @@ def combine_gan(g_model, d_model):
 
     # define gan model as taking noise and label and outputting a classification
     model = Model([gen_noise, gen_label], gan_output)
-    # compile model
-    opt = keras.optimizers.Adam(lr=1e-5)
-    model.compile(loss='binary_crossentropy',  # we change loss function here. feature_matiching
-                  optimizer=opt, metrics='accuracy')
     return model
 
 
-def generate_latent_points2(noise_vec, n_samples, n_classes=2):
+def generate_latent_points(noise_vec, n_samples, n_classes=2):
     # get random variables from a normal distribution with mu=0 and sigma=1.
     x_input = normal(0, 1, noise_vec * n_samples)
     # reshape into a batch of inputs for the network
@@ -186,53 +158,8 @@ def generate_latent_points2(noise_vec, n_samples, n_classes=2):
     return [z_input, labels]
 
 
-def train(g_model, d_model, gan_model, dataset, labels, laten_dim, n_epochs=10, n_batch=64):
-    bat_per_epo = int(dataset.shape[0] / n_batch)
-    half_batch = int(n_batch / 2)
-
-    for i in range(n_epochs):
-        # enumerate batches over the training set
-        for j in range(bat_per_epo):
-            # get randomly selected 'real' samples
-            [X_real, labels_real], y_real = generate_real_samples2(
-                dataset, labels, half_batch)
-            # update discriminator model weights
-            [X_fake, labels], y_fake = generate_fake_samples2(
-                g_model, laten_dim, half_batch)
-            d_loss1, _ = d_model.train_on_batch([X_real, labels_real], y_real)
-            d_loss2, _ = d_model.train_on_batch([X_fake, labels], y_fake)
-            # prepare points in latent space as input for the generator ##### z_input
-            [z_input, labels_input] = generate_latent_points2(
-                laten_dim, n_batch)
-            y_gan = np.ones((n_batch, 1))
-            _, g_loss = gan_model.train_on_batch(
-                [z_input, labels_input], y_gan)
-
-            # summarize loss on this batch
-            print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
-                  (i+1, j+1, bat_per_epo, d_loss1, d_loss2, g_loss))
-    # save the generator model
-    g_model.save('cgan_generator2.h5')
-
-
 def main():
-    laten_dim = 50
-    # create the discriminator
-    d_model = define_discriminator()
-    # create the generator
-    g_model = define_generator(laten_dim)
-    # create the gan
-    gan_model = combine_gan(g_model, d_model)
-
-    columns_ = df.columns[2:30]
-    dataset = df[columns_]  # X
-    labels = df.Class  # y.Class
-
-    # train model
-    print('dataset:', dataset)
-    print(np.shape(dataset))
-
-    train(g_model, d_model, gan_model, dataset, labels, laten_dim)
+    print("This is a library, you should use it as that, rather than running it!")
 
 
 if __name__ == "__main__":
